b630d945dfabcfd0ce3c232bcc0bbba2c85a3f8e
wangyu <451336451@qq.com>
2017-08-29 11:04:56 +0800

    Signed-off-by: wangyu <451336451@qq.com>

--- src/components/my/index.vue
+++ src/components/my/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="my">
+  <v-routers></v-routers>
   <wd-detail></wd-detail>
   </div>
 </template>
 
 <script>
+import routers from '../routers'
 import wd_detail from './wd-detail'
 export default {
   name: 'my',
   components:{
-  	'wd-detail':wd_detail
+  	'wd-detail':wd_detail,'v-routers':routers
   }
 }
 </script>
--- src/components/notice/notice_xq.vue
+++ src/components/notice/notice_xq.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="notice_xq">
-  <a>asdasdasdas</a>
+	  <div class="notice_not_head">
+      <a class="notice_not_head_img"><img src="../../assets/tx.png"></a>
+      <a class="notice_not_head_message">
+        <div><span class="ft-bold">万晓</span></div>
+        <div><span class="ft-12 ft-color-gray">今天 8:30</span></div>
+      </a>
+      <a class="notice_not_btn"><span></span><span></span><span></span></a>
+      <div class="clearbt"></div>
+    </div>
   </div>
 </template>
 
@@ -16,4 +24,43 @@ export default {
 </script>
 
 <style>
+#notice_xq > div{
+	margin:0em .75em;
+}
+.notice_not_head{
+  height: 3em;
+}
+.notice_not_head_img{
+  height:3em;
+  width: 3em;
+  float: left;
+}
+.notice_not_head_img img{
+  width:100%;
+  border-radius: .2em;
+}
+.notice_not_head_message{
+  float: left;
+  height:2.5em;
+  margin-left: .5em;
+  padding:.3em 0em .2em 0em;
+  text-align: left;
+}
+.notice_not_btn{
+  display: inline-block;
+  height:2.5em;
+  padding:.3em 0em .2em 0em;
+  vertical-align: top;
+  float: right;
+}
+.notice_not_btn > span{
+  display: inline-block;
+  border:1px solid #c1c1c1;
+  background: #c1c1c1;
+  border-radius: 50%;
+  width:.2em;
+  height: .2em;
+  margin-right: .2em;
+  vertical-align:text-top;
+}
 </style>
